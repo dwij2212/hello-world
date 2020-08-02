b8da0879f252b9918bdb2a47e26f1419c673d594
Dwij <dwij.mehta@gmail.com>
2020-08-02 18:06:27 +0530

    Adding 2 numbers cpp file

---
+++ C++/add.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	if (argc == 0)
+	{
+		cout<<"Enter 2 numbers";
+		return 1;
+	}
+	else if (argc == 1)
+	{
+		cout<<"Enter atleast 2 numbers";
+		return 1;
+	}
+
+	int sum = 0;
+	for (int i = 1; i < argc; ++i)
+	{
+		/* code */
+		sum += atoi(argv[i]);
+	}
+	cout<<sum;
+	return 0;
+}
